b086346f24d280a0b720db1860163f09ef0f1421
matteo.mascellani <matteo.mascellani@gmail.com>
2023-06-19 18:56:38 +0200

    fix

--- public/index.js
+++ public/index.js
@@ -310,6 +310,7 @@ $(function() {
                 success : function(data) {
                     var obj = jQuery.parseJSON(data);
                     console.log(obj.is_ean);
+                    $("#result_strip ul.thumbnails").html(obj.is_ean ? "Prodotto presente nell'ordine" : "Prodotto non presente nell'ordine");
                     $("#result_strip ul.thumbnails").prepend($node);
                 },
                 error : function() {
